80bcbc29a34657992192dc405f85d54bd5a1038a
unknown <sergio.gamboa11@gmail.com>
2024-07-06 21:40:29 -0600

    feat(shop): update shop and product components to display products properly

--- all-the-merch/src/pages/shop/Product.jsx
+++ all-the-merch/src/pages/shop/Product.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import "./Product.css"
 
 export const Product = (props) => {
     const { id, key, name, image, price } = props.data;
     return (
-        <div>
+        <div className="product-container">
+            <img className="product-image" src={image} />
             <div>{name}</div>
-            <div>{image}</div>
-            <div>{price}</div>
+            <div>${price}</div>
         </div>
     )
 }
